e6640c6ceb4e33130fef5643c48005c89c5b7670
iipekolict <iipekolict@gmail.com>
2022-01-26 23:30:48 +0300

    f3

--- cv/settings.py
+++ cv/settings.py
@@ -138,8 +138,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-MEDIA_URL = 'static/'
+MEDIA_URL = 'media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(locals())
